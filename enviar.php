3f0d107ed9b6897aa1200a703d4a6ca91b3c71c8
Pablo Duran Torres <pabloduran1997@hotmail.com>
2018-05-20 15:47:31 -0500

    empezando portada

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+  
+/*  $destino = "pablo.durant@utadeo.edu.co";
+  $nombre = $_POST['nombre'];
+  $asunto = $_POST['asunto'];
+  $email = $_POST['email'];
+  $mensaje = $_POST['mensaje'];
+  $contenido = "Nombre: " .$nombre. "\nCorreo: " .$email. "\nMensaje: " .$mensaje;
+  
+  mail($destino,$asunto,$contenido);
+  header("Location:contacto-enviado.html");*/
+
+?>
